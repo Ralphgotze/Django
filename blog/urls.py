bbe1a5f13be6fb64afb2d6699aa813b48a8be66c
Ralph Gotze <66144653+Ralphgotze@users.noreply.github.com>
2022-03-12 13:56:13 -0300

    Forms

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,12 @@
+from importlib import import_module
+from unicodedata import name
 from django.urls import  path
 from .views import BlogListView
+from .views import BlogCreateView, BlogListView
 
 app_name="blog"
 
 urlpatterns = [
-    path('',BlogListView.as_view(), name="home")
+    path('',BlogListView.as_view(), name="home"),
+    path('create/',BlogCreateView.as_view(), name="home")
 ]
