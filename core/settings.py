779897bdda408785800eefe62754c2d42058d9ce
Ralph Gotze <66144653+Ralphgotze@users.noreply.github.com>
2022-03-13 17:50:39 -0300

    finish

--- core/settings.py
+++ core/settings.py
@@ -34,7 +34,15 @@ INSTALLED_APPS = [
     
     'core',
     'blog',
+
+    'tailwind',
+    'theme',
 ]
+
+TAILWIND_APP_NAME = 'theme'
+INTERNAL_IPS = {
+    "127.0.0.1",
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
