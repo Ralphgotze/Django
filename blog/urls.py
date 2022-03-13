e37607c8cc1e294a935a0fc06cf44e794c3f0a58
Ralph Gotze <66144653+Ralphgotze@users.noreply.github.com>
2022-03-13 16:38:45 -0300

    Crud

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,15 @@
 from importlib import import_module
 from unicodedata import name
 from django.urls import  path
-from .views import BlogListView
-from .views import BlogCreateView, BlogListView
+from .views import BlogListView, BlogUpdateView
+from .views import BlogCreateView, BlogListView, BlogDetailView,BlogUpdateView,BlogDeleteView
 
 app_name="blog"
 
 urlpatterns = [
     path('',BlogListView.as_view(), name="home"),
-    path('create/',BlogCreateView.as_view(), name="create")
+    path('create/',BlogCreateView.as_view(), name="create"),
+    path('<int:pk>',BlogDetailView.as_view(),name="detail"),
+    path('<int:pk>/update/',BlogUpdateView.as_view(),name="update"),
+    path('<int:pk>/delete/',BlogDeleteView.as_view(),name="delete")
 ]
