0a465948656791c6a47f2e91aa17a41ba3839411
Ralph Gotze <66144653+Ralphgotze@users.noreply.github.com>
2022-03-23 20:46:05 -0300

    si

--- users/models.py
+++ users/models.py
@@ -1,35 +1,26 @@
-from datetime import datetime
-from tkinter.tix import Tree
-from unittest.util import _MAX_LENGTH
 from django.db import models
 
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
 
-class User(models.Model):
-    fisrt_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    cars = models.ManyToManyField('Car',verbose_name="los carros del usuario")
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
 
-STATUS_CHOICES = {
-    ('R', 'Reviewed'),
-    ('N', 'Not Reviewed'),
-    ('E', 'Error'),
-    ('A', 'Accepted')
-}
+    def __str__(self):
+        return self.name
 
-class Website(models.Model):
-    name = models.CharField(max_length=50)
-    url = models.URLField()
-    relase_date = models.DateField()
+class Entry(models.Model):
+    blog = models.ForeignKey(Blog,on_delete=models.CASCADE)
+    headline = models.CharField(max_length=255)
+    body_text = models.TextField()
+    pub_date = models.DateField()
+    mod_date = models.DateField()
+    authors = models.ManyToManyField(Author)
+    n_comments = models.IntegerField()
+    n_pingbacks = models.IntegerField()
     rating = models.IntegerField()
-    status = models.CharField(choices=STATUS_CHOICES, max_length=1)
-    owner = models.ForeignKey(User,on_delete=models.CASCADE)
 
-    def was_relased_last_week(self):
-        if self.relase_date < datetime.date(2020,6,6):
-            return "Yeah"
-    @property
-    def get_full_name(self):
-        return "Este es el nombre de la pagina: {name}"
-
-class Car(models.Model):
-    name = models.CharField(max_length=40,primary_key=True)
+    def __str__(self):
+        return self.headline
