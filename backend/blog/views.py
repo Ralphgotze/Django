248d29e2774c67e12693f2c24a66f3fc0f52c585
Ralph Gotze <66144653+Ralphgotze@users.noreply.github.com>
2022-03-29 06:08:35 -0400

    blog

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,6 +1,8 @@
 import imp
+from django import views
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views.generic.detail import DetailView
 from .models import Post
 
 
@@ -12,3 +14,6 @@ class BlogHomePageView(TemplateView):
         context["posts"] = Post.Postobjects.all()
         return context
 
+class PostDetailView(DetailView):
+    model = Post
+    template = 'blog/post-detail.html'
